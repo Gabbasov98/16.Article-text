6a625f74c0ec11f504b0b9b3b180f47ac1ab926a
Marsel Gabbasov <marsel13_98@mail.ru>
2021-08-04 16:51:14 +0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
-// const { Chart } = require("./chart.min");
-
-// const { Chart } = require("./chart.min");
-
 $(document).ready(function() {
     $(" .select").niceSelect()
-
 
     $(".table__check input").click(function() {
         if ($(this).is(':checked')) {
@@ -26,9 +21,15 @@ $(document).ready(function() {
                 div.siblings(".datapicker__show").removeClass("datapicker__show--active")
             }
         }
-
     });
-    if (document.getElementById('myChart')) {
+
+    $(".table__head-title").click(function() {
+        console.log(true)
+        $(this).toggleClass("table__head-title--active")
+    })
+
+
+    function doughnutDiagram() {
         var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'doughnut',
@@ -36,29 +37,26 @@ $(document).ready(function() {
                 // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [{
                     label: '# of Votes',
-                    data: [20, 20, 15, 7],
+                    data: [20, 20, 15],
                     backgroundColor: [
                         'rgba(111, 82, 237, 1)',
                         'rgba(255, 184, 0, 1)',
-                        'rgba(255, 76, 97, 1)',
                         'rgba(51, 214, 159, 1)',
-                        // 'rgba(255, 159, 64, 0.2)'
                     ],
                     borderColor: [
                         'rgba(111, 82, 237, 1)',
                         'rgba(255, 184, 0, 1)',
-                        'rgba(255, 76, 97, 1)',
                         'rgba(51, 214, 159, 1)',
-                        // 'rgba(255, 159, 64, 1)'
                     ],
-                    // borderWidth: 1
                 }]
             },
             options: {
                 cutout: 50
             }
         });
-    } else {
+    }
+
+    function doughnutDiagram2() {
         var ctx = document.getElementById('myChart2').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'doughnut',
@@ -89,51 +87,10 @@ $(document).ready(function() {
         });
     }
 
-
     function lineDiagram() {
-        // Chart.defaults.scale.display = false
-
-        // var speedCanvas = document.getElementById("myChart3");
-
-        // // Chart.defaults.global.defaultFontFamily = "Lato";
-        // // Chart.defaults.global.defaultFontSize = 18;
-
-        // var speedData = {
-        //     labels: ["Янв", "10s", "20s", "30s", "40s", "50s", "60s"],
-        //     datasets: [{
-        //         label: "Статистика просмотров",
-        //         // label: false,
-        //         data: [0, 59, 75, 20, 20, 55, 40],
-        //         lineTension: 1,
-        //         fill: true,
-        //         backgroundColor: 'rgba(55, 81, 255, 0.1)', // Цвет фона
-        //         borderColor: 'rgba(0, 73, 141, 1)', // Цвет границы
-        //         borderWidth: 2,
-        //     }]
-        // };
-
-        // var chartOptions = {
-        //     legend: {
-        //         display: false,
-        //         // position: 'top left',
-        //         // labels: {
-        //         //     boxWidth: 0,
-        //         //     fontColor: 'yellow'
-        //         // },
-
-        //     }
-        // };
-
-        // var lineChart = new Chart(speedCanvas, {
-        //     type: 'line',
-        //     data: speedData,
-        //     options: chartOptions
-        // });
-        // var ctx = document.getElementById('myChart2').getContext('2d');
         Chart.defaults.scale.beginAtZero = true;
         Chart.defaults.scale.ticks.maxRotation = 0;
         Chart.defaults.scale.ticks.backdropColor = 'rgba(159, 162, 180,1)'
-            // Chart.defaults.scale.ticks.padding = 10;
         Chart.defaults.font.size = 10;
         Chart.defaults.font.family = 'Proxima Nova';
         Chart.defaults.plugins.legend.display = false;
@@ -170,7 +127,13 @@ $(document).ready(function() {
         })
     }
 
-    lineDiagram()
-
-
+    if (document.getElementById('myChart')) {
+        doughnutDiagram()
+    }
+    if (document.getElementById('myChart2')) {
+        doughnutDiagram2()
+    }
+    if (document.getElementById('myChart3')) {
+        lineDiagram()
+    }
 })
