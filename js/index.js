7e371bcb4ea4789875d1d8a94370d255c926d223
Marsel Gabbasov <marsel13_98@mail.ru>
2021-08-04 17:57:31 +0500

    3

--- js/index.js
+++ js/index.js
@@ -4,7 +4,10 @@
 
 $(document).ready(function() {
     $(" .select").niceSelect()
-
+    $(".table__head-title").click(function() {
+        console.log(true)
+        $(this).toggleClass("table__head-title--active")
+    })
 
     $(".table__check input").click(function() {
         if ($(this).is(':checked')) {
@@ -28,7 +31,9 @@ $(document).ready(function() {
         }
 
     });
-    if (document.getElementById('myChart')) {
+
+
+    function doughnutDiagram() {
         var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'doughnut',
@@ -58,7 +63,9 @@ $(document).ready(function() {
                 cutout: 50
             }
         });
-    } else {
+    }
+
+    function doughnutDiagram2() {
         var ctx = document.getElementById('myChart2').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'doughnut',
@@ -89,47 +96,8 @@ $(document).ready(function() {
         });
     }
 
-
     function lineDiagram() {
-        // Chart.defaults.scale.display = false
 
-        // var speedCanvas = document.getElementById("myChart3");
-
-        // // Chart.defaults.global.defaultFontFamily = "Lato";
-        // // Chart.defaults.global.defaultFontSize = 18;
-
-        // var speedData = {
-        //     labels: ["Янв", "10s", "20s", "30s", "40s", "50s", "60s"],
-        //     datasets: [{
-        //         label: "Статистика просмотров",
-        //         // label: false,
-        //         data: [0, 59, 75, 20, 20, 55, 40],
-        //         lineTension: 1,
-        //         fill: true,
-        //         backgroundColor: 'rgba(55, 81, 255, 0.1)', // Цвет фона
-        //         borderColor: 'rgba(0, 73, 141, 1)', // Цвет границы
-        //         borderWidth: 2,
-        //     }]
-        // };
-
-        // var chartOptions = {
-        //     legend: {
-        //         display: false,
-        //         // position: 'top left',
-        //         // labels: {
-        //         //     boxWidth: 0,
-        //         //     fontColor: 'yellow'
-        //         // },
-
-        //     }
-        // };
-
-        // var lineChart = new Chart(speedCanvas, {
-        //     type: 'line',
-        //     data: speedData,
-        //     options: chartOptions
-        // });
-        // var ctx = document.getElementById('myChart2').getContext('2d');
         Chart.defaults.scale.beginAtZero = true;
         Chart.defaults.scale.ticks.maxRotation = 0;
         Chart.defaults.scale.ticks.backdropColor = 'rgba(159, 162, 180,1)'
@@ -170,6 +138,12 @@ $(document).ready(function() {
         })
     }
 
+    if (document.getElementById('myChart')) {
+        doughnutDiagram()
+    }
+    if (document.getElementById('myChart2')) {
+        doughnutDiagram2()
+    }
     lineDiagram()
 
 
